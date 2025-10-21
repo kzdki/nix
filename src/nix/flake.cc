23c1bc87e32537e83eccace99536f73f800dec84
Robert Hensing <robert@roberthensing.nl>
2025-10-22 01:03:31 +0200

    Remove "(ignored)" from errors in nix flake check --keep-going

--- src/nix/flake.cc
+++ src/nix/flake.cc
@@ -362,7 +362,7 @@ struct CmdFlakeCheck : FlakeCommand
                 throw;
             } catch (Error & e) {
                 if (settings.keepGoing) {
-                    ignoreExceptionExceptInterrupt();
+                    logError(e.info());
                     hasErrors = true;
                 } else
                     throw;
