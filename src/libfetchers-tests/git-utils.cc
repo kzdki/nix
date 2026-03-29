61f4f6d010be3e74f8988fa602a203e48e40e29c
Sergei Zimmerman <sergei@zimmerman.foo>
2026-03-29 23:46:16 +0300

    Make unity builds work

--- src/libfetchers-tests/git-utils.cc
+++ src/libfetchers-tests/git-utils.cc
@@ -15,7 +15,7 @@
 #include <git2-experimental/blob.h>
 #include <git2-experimental/tree.h>
 
-namespace nix {
+namespace nix::fetchers {
 
 class GitUtilsTest : public ::testing::Test
 {
@@ -234,4 +234,4 @@ TEST(GitUtils, isLegalRefName)
     ASSERT_FALSE(isLegalRefName(""));
 }
 
-} // namespace nix
+} // namespace nix::fetchers
