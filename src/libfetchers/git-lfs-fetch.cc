1aa2a673523cb10adb0750faf27f6d678f287f2b
Eelco Dolstra <edolstra@gmail.com>
2026-04-17 16:04:02 +0200

    Move Deleter into a separate header

--- src/libfetchers/git-lfs-fetch.cc
+++ src/libfetchers/git-lfs-fetch.cc
@@ -8,6 +8,7 @@
 #include "nix/util/util.hh"
 #include "nix/util/hash.hh"
 #include "nix/store/ssh.hh"
+#include "nix/util/deleter.hh"
 
 #include <git2-experimental/attr.h>
 #include <git2-experimental/config.h>
--- src/libfetchers/git-utils.cc
+++ src/libfetchers/git-utils.cc
@@ -13,6 +13,7 @@
 #include "nix/util/util.hh"
 #include "nix/util/thread-pool.hh"
 #include "nix/util/pool.hh"
+#include "nix/util/deleter.hh"
 
 #include <git2-experimental/attr.h>
 #include <git2-experimental/blob.h>
