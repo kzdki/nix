DeterminateSystems/nix-src

Add setting narinfo-cache-meta-ttl
This makes the current hard-coded 7-day `nix-cache-info` TTL
configurable, making `--offline` and `--refresh` do the right thing.

src/libstore/nar-info-disk-cache.cc
@@ -67,9 +67,6 @@ public:
     /* How often to purge expired entries from the cache. */
     const int purgeInterval = 24 * 3600;
 
-    /* How long to cache binary cache info (i.e. /nix-cache-info) */
-    const int cacheInfoTtl = 7 * 24 * 3600;
-
     struct Cache
     {
         int id;
@@ -184,7 +181,7 @@ private:
     {
         auto i = state.caches.find(uri);
         if (i == state.caches.end()) {
-            auto queryCache(state.queryCache.use()(uri)(time(0) - cacheInfoTtl));
+            auto queryCache(state.queryCache.use()(uri)(time(0) - settings.ttlNarInfoCacheMeta));
             if (!queryCache.next())
                 return std::nullopt;
             auto cache = Cache{

kzdki/nix

src/libstore/nar-info-disk-cache.cc
@@ -69,9 +69,6 @@ struct NarInfoDiskCacheImpl : NarInfoDiskCache
     /* How often to purge expired entries from the cache. */
     const int purgeInterval = 24 * 3600;
 
-    /* How long to cache binary cache info (i.e. /nix-cache-info) */
-    const int cacheInfoTtl = 7 * 24 * 3600;
-
     struct Cache
     {
         int id;
@@ -190,7 +187,7 @@ private:
     {
         auto i = state.caches.find(uri);
         if (i == state.caches.end()) {
-            auto queryCache(state.queryCache.use()(uri)(time(0) - cacheInfoTtl));
+            auto queryCache(state.queryCache.use()(uri)(time(0) - settings.ttlMeta));
             if (!queryCache.next())
                 return std::nullopt;
             auto cache = Cache{